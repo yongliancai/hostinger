00554d61bf4c08cf7f0f604ea4c3fcb500c4d352
yongliancai <74762143+yongliancai@users.noreply.github.com>
2024-03-23 10:48:09 +0800

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./app.js",
+      env: {
+        "OPENAI_API_KEY": "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"
+      }
+    }]
+  };
+  
